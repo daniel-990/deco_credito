acbce6384a69603b0163a81dc5e19272a7ed6043
Daniel Arango <danielarango990@gmail.com>
2022-07-06 09:29:23 -0500

    se modifican las credenciales
    Se modifican las credenciales de acceso

--- constantes/credenciales.php
+++ constantes/credenciales.php
@@ -1,31 +1,18 @@
 <?php 
 
         //base de datos local remota 1
-        define('SERVER','beukmwk8bhjymx9xyynh-mysql.services.clever-cloud.com');
-        define('USER','uhr46lhlxvzli1f8');
-        define('PASS','7uVGBmseYGcem8GvQL6x');
-        define('DB','beukmwk8bhjymx9xyynh');
-
-        //base de datos local remota 2
-        // define('SERVER','bjv5du2ytluyo6emozwn-mysql.services.clever-cloud.com');
-        // define('USER','ufh12kh9mriwsvff');
-        // define('PASS','vxmICHf2zUW2h9Ewne8a');
-        // define('DB','bjv5du2ytluyo6emozwn');
+        define('SERVER','');
+        define('USER','');
+        define('PASS','');
+        define('DB','');
 
         /**
          * API:
          * -----
-	 * https://api.telegram.org/bot5236648591:AAHHgtFWIMYQ0JNp-wQbDEpnSs9HZQIrTjE/getUpdates
+	 * https://api.telegram.org/botXXX:XXX/getUpdates
 	 */
         
         //bot
-        define('API','5236648591:AAHHgtFWIMYQ0JNp-wQbDEpnSs9HZQIrTjE');
-        define('USERID',177279577);
-
-        //base de datos remota
-        // define('SERVER','https://decohouse.com.co');
-        // define('USER','decohous_tienda2');
-        // define('PASS','s=3UMb~I)s&(');
-        // define('DB','decohous_db_decocredito');
-
+        define('API','');
+        define('USERID',000);
 ?>
